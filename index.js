b59632a9a7d20d01ab07ff0ad0afd3c7c6fe564f
jenboyd <jenniferlboyd1@gmail.com>
2017-09-18 15:36:14 -0400

    Work on adding in carousel

---
+++ index.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+ $(".owl-carousel").owlCarousel({
+   items: 2,
+   loop: true,
+   margin: 0,
+   lazyLoad: true,
+   autoplay: 20000,
+   autoplayTimeout: 5000,
+ });
+});
