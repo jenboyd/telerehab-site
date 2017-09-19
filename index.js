e46396fecc9658b5235e18371f624d37ec9a9516
jenboyd <jenniferlboyd1@gmail.com>
2017-09-19 15:28:32 -0400

    Add js for mobile nav

--- index.js
+++ index.js
@@ -6,5 +6,35 @@ $(document).ready(function() {
    lazyLoad: true,
    autoplay: 20000,
    autoplayTimeout: 5000,
+   responsiveClass:true,
+   responsive:{
+      0:{
+          items: 1,
+          nav: false
+      },
+      901:{
+          items: 2,
+          nav: false
+      }
+   }
  });
+
+  let isActive = false;
+
+  $('.mobile-nav-button').on('click', function() {
+    	if (isActive) {
+    		$(this).removeClass('active');
+    		$('body').removeClass('menu-open');
+    	} else {
+    		$(this).addClass('active');
+    		$('body').addClass('menu-open');
+    	}
+    	isActive = !isActive;
+  });
+
+  $('.option').on('click', function() {
+      $('.mobile-nav-button').removeClass('active');
+      $('body').removeClass('menu-open');
+      isActive = !isActive;
+  });
 });
